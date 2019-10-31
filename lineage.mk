5e1b4c631ed07590fb25b1e84fa096a846ea2b0b
Hiru <rlawoehd187@naver.com>
2019-10-31 15:00:50 +0900

    ef52: cmhw -> lineagehw

--- lineage.mk
+++ lineage.mk
@@ -8,7 +8,7 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Inherit device configuration
 $(call inherit-product, device/pantech/ef52/full_ef52.mk)
 
-BOARD_HARDWARE_CLASS := device/pantech/ef52/cmhw/
+BOARD_HARDWARE_CLASS := device/pantech/ef52/lineagehw/
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ef52

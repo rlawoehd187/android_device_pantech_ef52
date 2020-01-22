065a795ec354da7de0edc81e21d924c80d19e816
hiru <rlawoehd187@naver.com>
2020-01-23 08:53:58 +0900

    ef52: rework lineage.mk

--- lineage.mk
+++ lineage.mk
@@ -1,8 +1,24 @@
+#
+# Copyright (C) 2015-2016 The CyanogenMod Project
+#               2017-2018 The LineageOS Projec
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1080
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common BlurOS stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
@@ -10,19 +26,19 @@ $(call inherit-product, device/pantech/ef52/full_ef52.mk)
 
 # BOARD_HARDWARE_CLASS := device/pantech/ef52/lineagehw/
 
-## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ef52
 PRODUCT_NAME := lineage_ef52
 PRODUCT_BRAND := Vega
 PRODUCT_MODEL := Vega Iron
 PRODUCT_MANUFACTURER := Vega
 
-# Set build fingerprint / ID / Product Name ect.
-#PRODUCT_BUILD_PROP_OVERRIDES += \
-#    PRODUCT_NAME="Vega Iron" \
-#    TARGET_DEVICE=ef52 \
+TARGET_VENDOR := Pantech
+TARGET_VENDOR_PRODUCT_NAME := ef52
+TARGET_VENDOR_DEVICE_NAME := ef52
 
+PRODUCT_GMS_CLIENTID_BASE := android-pantech
 
-# Enable Torch
-#PRODUCT_PACKAGES += Torch
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="msm8960-user 4.4.2 KOT49H IM-A870K.010 release-keys" 
 
+BUILD_FINGERPRINT := VEGA/VEGA_IM-A870K/ef52k:4.4.2/KOT49H/IM-A870K.010:user/release-keys
